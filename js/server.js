e0923ebe0cf0897f7d676b90e12b8a644003f701
AnthonyTheSabakumono <altanorgil98@gmail.com>
2026-03-23 15:48:53 +0800

    Serve static files from the 'js' directory
    Updated static file serving to include the 'js' folder.

--- js/server.js
+++ js/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-// Serve all static files in the current folder
+// Serve the js folder
+app.use('/js', express.static(path.join(__dirname, 'js')));
+
+// Serve HTML files in the root
 app.use(express.static(__dirname));
 
 const PORT = process.env.PORT || 3000;
